c374e80fc7d4081652384d0a81765d3cf1f68ff2
Muhammad Huzaifa Khan <huzaifa.khan@tpsonline.com>
2023-02-04 13:37:44 +0500

    removed

--- flutter_bloc_example/lib/blocs/internet_bloc/internet_state.dart
+++
@@ -1,7 +0,0 @@
-abstract class InternetState {}
-
-class InternetInitialState extends InternetState {}
-
-class InternetLossState extends InternetState {}
-
-class InternetGainedState extends InternetState {}

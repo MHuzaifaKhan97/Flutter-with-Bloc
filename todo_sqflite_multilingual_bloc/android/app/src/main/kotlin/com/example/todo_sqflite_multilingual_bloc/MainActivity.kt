474afd86065829673914478b87a78becab1d2787
Muhammad Huzaifa Khan <huzaifa.khan@tpsonline.com>
2023-05-02 00:59:44 +0500

    Project Init.

---
+++ todo_sqflite_multilingual_bloc/android/app/src/main/kotlin/com/example/todo_sqflite_multilingual_bloc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.todo_sqflite_multilingual_bloc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ todo_sqflite_multilingual_bloc/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo Multilingual with SQFlite',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
